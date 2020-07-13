23ca9c93208b01588a910c41c6f6c56e94f57dcc
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-07-12 23:34:48 -0400

    #self-info added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxTypedJsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
