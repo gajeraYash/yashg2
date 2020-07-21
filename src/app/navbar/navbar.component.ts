fe92c150f7398599ad836b5a34559a564b686d9f
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 12:47:05 -0400

    smooth scroll jquery

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 declare const Waypoint: any;
+import $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -11,9 +12,31 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    function scroll(el: HTMLElement) {
-      el.scrollIntoView({behavior:"smooth"})
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        },1000);
+      }
     }
+  });
     var waypoint = new Waypoint({
       element: document.getElementById('about'),
       handler: function(direction) {
