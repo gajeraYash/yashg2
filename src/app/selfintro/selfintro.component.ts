388d49f47adac0d738271509c6012432e5b8c273
BuildTools <gajera.yash98@gmail.com>
2020-08-09 10:58:33 -0400

    removed AOS INIT

--- src/app/selfintro/selfintro.component.ts
+++ src/app/selfintro/selfintro.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxTypedJsModule } from 'ngx-typed-js';
-import * as AOS from 'aos';
 @Component({
   selector: 'app-selfintro',
   templateUrl: './selfintro.component.html',
   styleUrls: ['./selfintro.component.css']
 })
-export class SelfintroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    AOS.init()
-  }
+export class SelfintroComponent {
 
 }
